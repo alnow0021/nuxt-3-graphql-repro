c6ce44bbdb1e9136b18596bb314e1d9de13bd8fd
alnow0021 <125440216+alnow0021@users.noreply.github.com>
2023-02-15 08:55:40 +0100

    Create [...slugs].vue

---
+++ pages/[...slugs].vue
@@ -0,0 +1,10 @@
+<template> example </template>
+
+<script lang="ts" setup>
+import { print } from 'graphql'
+import GET_EXAMPLE from '../query/getExample.gql'
+
+const stringQuery = print(GET_EXAMPLE)
+
+console.log(stringQuery)
+</script>
